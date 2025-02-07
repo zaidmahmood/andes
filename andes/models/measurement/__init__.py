8806069f3aab14f00b9ca92fc564a4fd022e24d6
zaidmahmood <zaid.mahmood@okstate.edu>
2025-02-06 18:04:50 -0600

    Merge issues

--- andes/models/measurement/__init__.py
+++ andes/models/measurement/__init__.py
@@ -6,8 +6,6 @@ from andes.models.measurement.busfreq import BusFreq  # noqa
 from andes.models.measurement.busrocof import BusROCOF  # noqa
 from andes.models.measurement.pmu import PMU   # noqa
 from andes.models.measurement.pll import PLL1, PLL2  # noqa
-<<<<<<< HEAD
-=======
 from andes.models.measurement.InertiaEstimationConstantPm import InertiaEstimationConstantPm #noqa
 from andes.models.measurement.InertiaEstimationConstPmREGCA1 import InertiaEstimationConstPmREGCA1 #noqa
 from andes.models.measurement.InertiaEstimationConstPmREGCV2 import InertiaEstimationConstPmREGCV2 #noqa
@@ -16,4 +14,3 @@ from andes.models.measurement.InertiaEstimationConstPmREGF2 import InertiaEstima
 from andes.models.measurement.InertiaEstimationLiu import InertiaEstimationLiu #noqa
 from andes.models.measurement.InertiaEstimationLiuWashout import InertiaEstimationLiuWashout #noqa
 from andes.models.measurement.InertiaEstimationVariablePm import InertiaEstimationVariablePm #noqa 
->>>>>>> 5fc2ef92... fixed bug in REGF1
